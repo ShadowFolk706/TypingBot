8c097084b41d4b1e4f586a29012cc3b5daa1f31e
ShadowFolk706 <107605578+ShadowFolk706@users.noreply.github.com>
2025-11-29 22:29:53 -0700

    Update TypingBot.py

--- TypingBot.py
+++ TypingBot.py
@@ -1,26 +1,29 @@
 import pyautogui
 import ollama
 import time
+import threading
 import random
 
-model = "llama2"
+model = "llama3.2"
+spinning = True
 
-def Type(String):
-    txtToPrint = String.split(" ")
+
+# Type Function
+def Type(String, wordThink, sentenceThink, dismissal):
+    txtToPrint = String.split(' ')
     times = [0.032, 0.035, 0.0375, 0.04]
 
-    print("You have 5 seconds to place your cursor")
+    print('You have 5 seconds to place your cursor', end='\r')
     time.sleep(1)
-    print("You have 4 seconds to place your cursor")
+    print('You have 4 seconds to place your cursor', end='\r')
     time.sleep(1)
-    print("You have but 3 seconds to place your cursor")
+    print('You have 3 seconds to place your cursor', end='\r')
     time.sleep(1)
-    print("You have 2 seconds to place your cursor")
+    print('You have 2 seconds to place your cursor', end='\r')
     time.sleep(1)
-    print("You have 1 seconds to place your cursor")
+    print('You have 1 second to place your cursor', end='\r')
     time.sleep(1)
 
-    pyautogui.typewrite("·", interval=.045)
     i = 0
 
     def quotationMark():
@@ -30,27 +33,78 @@ def Type(String):
 
     while i < len(txtToPrint):
         intrvl = random.choice(times)
-        # intrvl = 0
-        if txtToPrint[i]=='“':
-            quotationMark()
-        elif txtToPrint[i]=='”':
-            quotationMark()
-        else:
-            pyautogui.typewrite(txtToPrint[i], intrvl)
-        pyautogui.typewrite(" ", 0.00)
+        wordSplit = list(txtToPrint[i])
+        pyautogui.typewrite(txtToPrint[i], intrvl)
+        pyautogui.typewrite(' ', 0.02)
+        if wordSplit[-1]=='.':
+            if sentenceThink:
+                if random.randint(0,3) == 1:
+                    time.sleep(random.randint(15,30))
+        if wordThink:
+            if random.randint(0, 20) == 13:
+                time.sleep(random.randint(10,15))
         i += 1
+    if dismissal:
+        pyautogui.typewrite('\n')
+        pyautogui.typewrite('Done!')
 
 
+# Spinner Function
+def Spinner(stopEvent):
+    spinState=['⠹', '⠼', '⠶','⠧','⠏', '⠛']
+    i=0
+    while not stopEvent.is_set():
+        print(f'{spinState[i]}', end='\r')
+        time.sleep(.1)
+        i = (i+1)%len(spinState)
+    print(' ', end='\r')
+
+stopSpinner = threading.Event()
+
+
+# Main Running Loop
 while True:
-    aiOrNo = input("Would you like to use AI to answer this question [Y/N]: ")
-    if aiOrNo == "N":
-        textToType = input("What text would you like typed? ")
-        Type(textToType)
+    aiOrNo = input('Would you like to use AI to answer your question [Y/N]: ')
+    if aiOrNo != 'Y' and aiOrNo != 'N':
+        break
 
-    elif aiOrNo == "Y":
-        prompt = input("What question would you like AI to answer?: ")
-        aiAnswer = ollama.generate(model, prompt+' Do not include any special characters such as em dashes or emojis').response
-        Type(aiAnswer)
+    # Word Think
+    wordThinkYN = input('Would you like word think on? (waiting randomly between words) [Y/N]: ')
+    if wordThinkYN == 'Y':
+        wordThinkBool = True
+    elif wordThinkYN == 'N':
+        wordThinkBool = False
     else:
         break
 
+    # Sentence Think
+    sentenceThinkYN = input('Would you like sentence think on? (waiting randomly between sentences) [Y/N]: ')
+    if sentenceThinkYN  == 'Y':
+        sentenceThinkBool = True
+    elif sentenceThinkYN  == 'N':
+        sentenceThinkBool = False
+    else:
+        break
+
+    # Dismissal Message
+    dismissalYN = input('Would you like a „Done!“ when your text is done being typed? [Y/N]: ')
+    if dismissalYN == 'Y':
+        dissmissalBool = True
+    elif dismissalYN == 'N':
+        dissmissalBool = False
+    else:
+        break
+
+    # DO STUFF
+    if aiOrNo == 'N':
+        textToType = input('What text would you like typed? ')
+        Type(textToType, wordThinkBool, dissmissalBool)
+
+    elif aiOrNo == 'Y':
+        prompt = input('What question would you like AI to answer?: ')
+        spinnerThread = threading.Thread(target=Spinner, args=(stopSpinner,))
+        spinnerThread.start()
+        aiAnswer = ollama.generate(model, prompt+'. Do not include any special characters such as em dashes or emojis. Do not include a salutationary nor dismissal line nor a "Here is" statement at the beginning either.').response
+        stopSpinner.set()
+        spinnerThread.join()
+        Type(aiAnswer, wordThinkBool, sentenceThinkBool, dissmissalBool)
